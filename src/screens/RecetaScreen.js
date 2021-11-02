7a12e6a5ea0110010fd4c373f5193d4c06968c87
German Sommariva <germansommariva@gmail.com>
2021-11-01 22:20:47 -0300

    Cambios API Mockup inicial de RecetaScreen

--- src/screens/RecetaScreen.js
+++ src/screens/RecetaScreen.js
@@ -1,8 +1,30 @@
 //Para el ingreso de una receta en particular
 import React from 'react';
 import {Text,StyleSheet,View} from 'react-native';
+import RecetasAPI from '../utils/RecetasAPI';
+
 
 const RecetaScreen = () => {
+
+  //Funcion para usar con el boton guardar
+  const handleTituloChanged = () => {
+    //Actualizar el titulo state
+  }
+
+  const handleIngredientesChanged = () => {
+    //Actualizar el Ingredientes state
+  }
+
+  const handleDescripcionChanged = () => {
+    //Actualizar el Descripcion state
+  }
+
+  const handleGuardarPressed = async() => {
+    const recetaData = "" //Obtener la data de los states
+    await RecetasAPI.GuardarReceta(recetaData)
+    alert('Receta guardada') //Cambiar luego este alert por un Toast
+  }
+
   return(
     <View>
       <Text>Receta Screen</Text>
