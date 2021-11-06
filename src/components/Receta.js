48827b0e46c058539373a9bbc2a9472c250f57c4
German Sommariva <germansommariva@gmail.com>
2021-11-06 01:12:08 -0300

    Borrar receta

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View,Text,StyleSheet} from 'react-native';
+import {View,Text,StyleSheet, TouchableOpacity} from 'react-native';
+import {Entypo} from '@expo/vector-icons';
 
-const Receta = ({titulo,ingredientes,instrucciones}) => {
+const Receta = ({ titulo, ingredientes, instrucciones, borrar}) => {
   return(
     <View style={styles.container}>
-      <Text style={styles.titulo}>{titulo}</Text>
+      <View style={styles.header}>
+        <Text>{' '}</Text>
+        <Text style={styles.titulo}>{titulo}</Text>
+        <View style={styles.cross}>
+          <TouchableOpacity
+            style={styles.crossButton}
+            onPress={() => borrar(titulo)}
+          >
+            <Entypo name="cross" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
       <Text style={styles.texto}>{ingredientes}</Text>
       <Text style={styles.instrucciones}>{instrucciones}</Text>
     </View>
@@ -17,6 +29,16 @@ const styles = StyleSheet.create({
     padding:5,
     marginBottom:5,
     backgroundColor:'lightblue'
+  },
+  header:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+  crossButton:{
+    backgroundColor:'white',
+    padding:3,
+    borderRadius:20,
   },
   titulo:{
     fontSize:20,
--- src/screens/RecetasScreen.js
+++ src/screens/RecetasScreen.js
@@ -1,36 +1,44 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
-import { Text, View, TextInput, TouchableOpacity, ScrollView, StyleSheet} from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, ScrollView, StyleSheet, ToastAndroid } from 'react-native';
 import Receta from '../components/Receta';
 import RecetasAPI from '../utils/RecetasAPI';
 import Screens from '../utils/Screens';
 
-const RecetasScreen = ({navigation}) => {
-  const [recetas,setRecetas] = useState([]);
+const RecetasScreen = ({ navigation }) => {
+  const [recetas, setRecetas] = useState([]);
 
-  useFocusEffect(()=>{
+  useFocusEffect(() => {
     PopulateRecetas();
-  },[])
+  }, [])
 
-  const PopulateRecetas = async() => {
+  const PopulateRecetas = async () => {
     const recetasData = await RecetasAPI.ObtenerRecetas()
     setRecetas(recetasData);
   }
 
-  const handleAgregarReceta = async() => {
+  const handleAgregarReceta = async () => {
     navigation.navigate(Screens.RECETA)
   }
 
-  return(
+  const handleDelete = async (key) => {
+    const newRecetas = recetas.filter(receta => receta.titulo !== key);
+    await RecetasAPI.GuardarRecetas(newRecetas);
+    setRecetas(await RecetasAPI.ObtenerRecetas());
+    //Comentar el toast si se trabaja en la version web, sino la app crashea
+    ToastAndroid.show(`${key} borrado exitosamente.`, ToastAndroid.SHORT);
+  }
+  return (
     <View style={styles.container}>
       <View style={styles.recetasContainer}>
         <ScrollView style={styles.recetasScrollContainer}>
           {recetas.map(r => {
-            return <Receta 
-            key={r.titulo} 
-            titulo={r.titulo} 
-            ingredientes={r.ingredientes}
-            instrucciones={r.instrucciones}
+            return <Receta
+              key={r.titulo}
+              titulo={r.titulo}
+              ingredientes={r.ingredientes}
+              instrucciones={r.instrucciones}
+              borrar={handleDelete}
             />
           })}
         </ScrollView>
@@ -38,7 +46,7 @@ const RecetasScreen = ({navigation}) => {
       <View style={styles.menuContainer}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity
-            onPress={handleAgregarReceta}  
+            onPress={handleAgregarReceta}
             style={styles.button}
           >
             <Text style={styles.buttonText}>Agregar Receta</Text>
@@ -50,29 +58,29 @@ const RecetasScreen = ({navigation}) => {
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-  menuContainer:{
-    flex:1,
-    justifyContent:'center',
+  menuContainer: {
+    flex: 1,
+    justifyContent: 'center',
     marginHorizontal: '5%',
   },
-  recetasContainer:{
-    flex:2,
-    marginHorizontal:'5%',
+  recetasContainer: {
+    flex: 2,
+    marginHorizontal: '5%',
   },
-  button:{
-    padding:20,
-    marginHorizontal:30,
-    marginVertical:5,
-    backgroundColor:'black',
-    alignItems:'center'
+  button: {
+    padding: 20,
+    marginHorizontal: 30,
+    marginVertical: 5,
+    backgroundColor: 'black',
+    alignItems: 'center'
   },
-  buttonText:{
-    color:'white',
-    fontWeight:'bold',
-    fontSize:20,
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
   }
 })
 export default RecetasScreen;
