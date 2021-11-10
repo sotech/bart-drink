2b58a966f0a4e2b112f4cf2388ce0538252bfd09
German Sommariva <germansommariva@gmail.com>
2021-11-10 18:49:02 -0300

    Fix - Camera - Tomar foto

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,34 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect,useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { Camera } from 'expo-camera';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 
-const handleFotoPress=()=>{
-
-
-}
 
 const CameraScreen=()=>{
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
-
+  const camera = useRef(null)
+  
+  //Llama la funcion asincronica una vez al abrir la pantalla
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
   }, []);
-  
+
+  const handleFotoPress= async()=>{
+    console.log('Foto tomada')
+    if(camera){
+      const photo = await camera.current.takePictureAsync({
+        base64:true
+      });
+    }
+  }
+
+  //Renderizados
   if (hasPermission === null) {
     return <View />;
   }
+
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>No hay acceso a la camara</Text>;
   }
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera 
+      style={styles.camera} 
+      type={type}
+      ref={camera}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.button}
@@ -47,7 +60,7 @@ const CameraScreen=()=>{
       <View style={styles.buttonFoto}>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => {  }}>
+            onPress={handleFotoPress}>
             <Entypo name="camera" size={65} color="black"/>
           </TouchableOpacity>
          </View>
